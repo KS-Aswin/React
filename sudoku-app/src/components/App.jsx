50bae6235d9d3c3e4f4246c072e6ebcf94fd9259
Aswin K S <aswin.ks@techversantinfotech.com>
2024-07-26 18:00:15 +0530

    sudoku-app created

--- sudoku-app/src/components/App.jsx
+++ sudoku-app/src/components/App.jsx
@@ -1,48 +1,12 @@
-import { useState } from "react";
+import SudokuBoard from './SudokuBoard';
 import "../App.css";
 
 function App() {
-  const [grid, setGrid] = useState([
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]);
 
-  function setIndexValue(rowIndex, numberIndex, value) {
-    const newGrid = [...grid];
-    newGrid[rowIndex][numberIndex] = value;
-    setGrid(newGrid);
-  }
   return (
     <>
       <div className="App">
-        <div className="grid">
-          {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="row">
-              {row.map((number, numberIndex) => (
-                <div key={numberIndex} className="cell">
-                  <input
-                    type="text"
-                    value={number}
-                    onChange={(e) => {
-                      setIndexValue(
-                        rowIndex,
-                        numberIndex,
-                        parseInt(e.target.value)||0
-                      );
-                    }}
-                  />
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+        <SudokuBoard />
       </div>
     </>
   );
